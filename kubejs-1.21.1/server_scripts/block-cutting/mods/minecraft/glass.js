e2e6ee8b1a75ae5da69427c7b55530709f83f58d
l0py2 <l0py2.git@gmail.com>
2026-07-11 17:24:29 +0100

    Moved datapacks to a sub-directory and removed the global reference from glass block cutting script

--- kubejs-1.21.1/server_scripts/block-cutting/mods/minecraft/glass.js
+++ kubejs-1.21.1/server_scripts/block-cutting/mods/minecraft/glass.js
@@ -1,13 +1,34 @@
 //priority: 2
 
+(() => {
+const dyeColours = [
+	'white',
+	'light_gray',
+	'gray',
+	'black',
+	'brown',
+	'red',
+	'orange',
+	'yellow',
+	'lime',
+	'green',
+	'cyan',
+	'light_blue',
+	'blue',
+	'purple',
+	'magenta',
+	'pink'
+];
+
 BlockCutting.types.add('glass');
 
 BlockCutting.tags.push(['kubejs:blocks/glass', 'minecraft:glass']);
 BlockCutting.tags.push(['kubejs:panes/glass', 'minecraft:glass_pane']);
-	
-for(const colour of global.dyeColours) {
+
+for(const colour of dyeColours) {
 	BlockCutting.types.add(`${colour}_stained_glass`);
-	
+
 	BlockCutting.tags.push([`kubejs:blocks/${colour}_stained_glass`, `minecraft:${colour}_stained_glass`]);
 	BlockCutting.tags.push([`kubejs:panes/${colour}_stained_glass`, `minecraft:${colour}_stained_glass_pane`]);
 }
+})();
